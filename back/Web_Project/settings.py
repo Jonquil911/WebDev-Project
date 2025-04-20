c616b85c5066e7d9082f48bbbf62437f3d1a28fd
Айсумайсуу <you@example.com>
2025-04-20 10:22:22 +0500

    changes

--- back/Web_Project/settings.py
+++ back/Web_Project/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images))
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
