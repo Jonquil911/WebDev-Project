8d7939a3ca3c535fb4b38a5a61ece9079f706133
Айсумайсуу <you@example.com>
2025-04-20 10:22:22 +0500

    changes

--- back/api/views.py
+++ back/api/views.py
@@ -116,9 +116,6 @@ class CourseNotes(APIView):
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
