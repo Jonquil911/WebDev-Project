9b10995472212e091b8991cc6c07d926440f8695
Айсумайсуу <you@example.com>
2025-04-20 10:22:22 +0500

    changes

--- back/Web_Project/settings.py
+++ back/Web_Project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import datetime
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 from django.conf import settings
 
@@ -25,13 +24,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6k)teh83(g(ivdv-y42ba_mwe50qvfh8poqn@pq!_ngwa7#xw9'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
